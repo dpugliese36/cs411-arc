f5e7336c41879c923e98e9484c2a4733a41ce6f9
Daniel Pugliese <dpugliese@host-25-84.ilcujsm.champaign.il.us.clients.pavlovmedia.com>
2016-03-15 11:42:03 -0500

    debuggin form handler

--- createAccount.php
+++ createAccount.php
@@ -9,33 +9,36 @@
     $password = $_POST['password'];
     $password = hash("sha256", $password);
 
-    $mysqli = new mysqli("puglies2.web.engr.illinois.edu", "puglies2_tbd4", "arcarctbd4", "puglies2_arc");
-    if ($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    }
-
-    if (!($stmt = $mysqli->prepare("INSERT INTO User (NetId, Sex, Name, Birthday, Height, Weight, Password)"
-            . " VALUES (?, ?, ?, ?, ?, ?, ?)"))) {
-        echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-    }
-
-    $stmt->debugDumpParams();
-
-    if (!$stmt->bind_param("sssssis", $netID, $sex, $name, $bday, $height, $weight, $password)) {
-        echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-    }
-
-    $stmt->debugDumpParams();
-
-    if (!$stmt->execute()) {
-        echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-    }
-
-    // $result = $mysqli->query($stmt);
-
-    // if ($result === TRUE) {
-    //     echo "New record created successfully";
-    // } else {
-    //     echo "Error: " . $mysqli->error;
+    // $mysqli = new mysqli("puglies2.web.engr.illinois.edu", "puglies2_tbd4", "arcarctbd4", "puglies2_arc");
+    // if ($mysqli->connect_errno) {
+    //     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     // }
+
+    // if (!($stmt = $mysqli->prepare("INSERT INTO User (NetId, Sex, Name, Birthday, Height, Weight, Password)"
+    //         . " VALUES (?, ?, ?, ?, ?, ?, ?)"))) {
+    //     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    // }
+
+    // $stmt->debugDumpParams();
+
+    // if (!$stmt->bind_param("sssssis", $netID, $sex, $name, $bday, $height, $weight, $password)) {
+    //     echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    // }
+
+    // $stmt->debugDumpParams();
+
+    // if (!$stmt->execute()) {
+    //     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    // }
+
+    $stmt = "INSERT INTO User (NetId, Sex, Name, Birthday, Height, Weight, Password) "
+        . "VALUES ('{$netID}', '{$sex}', '{$name}', '{$bday}', '{$height}', '{$weight}', '{$password}';";
+
+    $result = $mysqli->query($stmt);
+
+    if ($result === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $mysqli->error;
+    }
 ?>
