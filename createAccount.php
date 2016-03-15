f41622109316713e48f91c383d34c90a4f0ea01c
Daniel Pugliese <dpugliese@host-25-84.ilcujsm.champaign.il.us.clients.pavlovmedia.com>
2016-03-15 11:45:41 -0500

    debuggin form handler

--- createAccount.php
+++ createAccount.php
@@ -14,31 +14,31 @@
         echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
 
-    // if (!($stmt = $mysqli->prepare("INSERT INTO User (NetId, Sex, Name, Birthday, Height, Weight, Password)"
-    //         . " VALUES (?, ?, ?, ?, ?, ?, ?)"))) {
-    //     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-    // }
-
-    // $stmt->debugDumpParams();
-
-    // if (!$stmt->bind_param("sssssis", $netID, $sex, $name, $bday, $height, $weight, $password)) {
-    //     echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-    // }
-
-    // $stmt->debugDumpParams();
-
-    // if (!$stmt->execute()) {
-    //     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-    // }
-
-    $stmt = "INSERT INTO User (NetId, Sex, Name, Birthday, Height, Weight, Password) "
-        . "VALUES ('{$netId}', '{$sex}', '{$name}', '{$bday}', '{$height}', '{$weight}', '{$password}');";
-
-    $result = $mysqli->query($stmt);
-
-    if ($result === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $mysqli->error;
+    if (!($stmt = $mysqli->prepare("INSERT INTO User (NetId, Sex, Name, Birthday, Height, Weight, Password)"
+            . " VALUES (?, ?, ?, ?, ?, ?, ?)"))) {
+        echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
     }
+
+    $stmt->debugDumpParams();
+
+    if (!$stmt->bind_param("sssssis", $netId, $sex, $name, $bday, $height, $weight, $password)) {
+        echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    $stmt->debugDumpParams();
+
+    if (!$stmt->execute()) {
+        echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    // $stmt = "INSERT INTO User (NetId, Sex, Name, Birthday, Height, Weight, Password) "
+    //     . "VALUES ('{$netId}', '{$sex}', '{$name}', '{$bday}', '{$height}', '{$weight}', '{$password}');";
+
+    // $result = $mysqli->query($stmt);
+
+    // if ($result === TRUE) {
+    //     echo "New record created successfully";
+    // } else {
+    //     echo "Error: " . $mysqli->error;
+    // }
 ?>
