73da430c63fed8a7b07c914ca75e62bac0cb0e6d
Daniel Pugliese <dpugliese@host-25-84.ilcujsm.champaign.il.us.clients.pavlovmedia.com>
2016-03-15 11:03:41 -0500

    Added submit button

--- signup.php
+++ signup.php
@@ -1,13 +1,14 @@
 <html>
     <body>
         <form action="createAccount.php" method="post">
-            netID: <input type="text" name="netID"/>
-            New Password: <input type="password" name="password"/>
-            Full Name: <input type="text" name="name"/>
-            Birthday: <input type="date" name="bday"/>
-            Sex: <input type="text" name="sex"/>
-            Height: <input type="number" name="height"/>
-            Weight: <input type="number" name="weight"/>
+            netID: <input type="text" name="netID"/><br/>
+            New Password: <input type="password" name="password"/><br/>
+            Full Name: <input type="text" name="name"/><br/>
+            Birthday: <input type="date" name="bday"/><br/>
+            Sex: <input type="text" name="sex"/><br/>
+            Height: <input type="number" name="height"/><br/>
+            Weight: <input type="number" name="weight"/><br/>
+            <input type="submit"/>
         </form>
     </body>
 </html>
