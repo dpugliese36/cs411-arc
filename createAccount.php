ff39617eacb612f63fb8150dcf43ec3a16589afe
Daniel Pugliese <dpugliese@host-25-84.ilcujsm.champaign.il.us.clients.pavlovmedia.com>
2016-03-14 21:48:07 -0500

    created a php script to create new accounts, no logic yet to check that account doesn't exist already

---
+++ createAccount.php
@@ -0,0 +1,27 @@
+<?php
+    $password = $_POST['password'];
+    $netId = $_POST['netID'];
+    $bday = $_POST['bday'];
+    $name = $_POST['name'];
+    $height = $_POST['height'];
+    $weight = $_POST['weight'];
+    $sex = $_POST['sex'];
+
+    $mysqli = new mysqli("puglies2.web.engr.illinois.edu", "puglies2_tbd4", "arcarctbd4", "puglies2_arc");
+    if ($mysqli->connect_errno) {
+        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+
+    if (!($stmt = $mysqli->prepare("INSERT INTO Users(NetId, Sex, Name, Birthday, Height, Weight, Password)"
+            + "VALUES ((1), (2), (3), (4), (5), (6), (7))"))) {
+        echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+
+    if (!$stmt->bind_param("sssssss", $netID, $sex, $name, $bday, $height, $weight, hash("sha256", $password))) {
+        echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    if (!$stmt->execute()) {
+        echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+?>
