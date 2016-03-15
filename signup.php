40a1723d9c2b8c4d7aad5d99005f4219dd281f64
Daniel Pugliese <dpugliese@host-25-84.ilcujsm.champaign.il.us.clients.pavlovmedia.com>
2016-03-15 11:00:58 -0500

    Added basic html form for signup

---
+++ signup.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <form action="createAccount.php" method="post">
+            netID: <input type="text" name="netID"/>
+            New Password: <input type="password" name="password"/>
+            Full Name: <input type="text" name="name"/>
+            Birthday: <input type="date" name="bday"/>
+            Sex: <input type="text" name="sex"/>
+            Height: <input type="number" name="height"/>
+            Weight: <input type="number" name="weight"/>
+        </form>
+    </body>
+</html>
