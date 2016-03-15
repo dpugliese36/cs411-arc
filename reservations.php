c4369fdee879184ef5b5cd3e491a4d98338c74eb
dougzhu1 <dougzhu1@gmail.com>
2016-03-14 23:46:53 -0500

    revert1

--- reservations.php
+++ reservations.php
@@ -8,12 +8,12 @@ echo "My first PHP script!";
 
 
 <?php
-    /*$mysqli = new mysqli("puglies2.web.engr.illinois.edu", "puglies2_tbd4", "arcarctbd4", "puglies2_arc");
+    $mysqli = new mysqli("puglies2.web.engr.illinois.edu", "puglies2_tbd4", "arcarctbd4", "puglies2_arc");
     if ($mysqli->connect_errno) {
         echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
 
-    if (!($stmt = $mysqli->prepare("INSERT INTO Users(NetId, Sex, Name, Birthday, Height, Weight, Password)"
+    /*if (!($stmt = $mysqli->prepare("INSERT INTO Users(NetId, Sex, Name, Birthday, Height, Weight, Password)"
             + "VALUES ((1), (2), (3), (4), (5), (6), (7))"))) {
         echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
     }
