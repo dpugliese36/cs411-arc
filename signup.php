ce2b590e7d6ab20a676e533315403fdc29fae1c0
Daniel Pugliese <dpugliese@wirelessprvnat-172-16-161-236.near.illinois.edu>
2016-03-17 12:18:08 -0500

    Changed height to text from number

--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@
             Full Name: <input type="text" name="name"/><br/>
             Birthday: <input type="date" name="bday"/><br/>
             Sex: <input type="text" name="sex"/><br/>
-            Height: <input type="number" name="height"/><br/>
+            Height: <input name="height"/><br/>
             Weight: <input type="number" name="weight"/><br/>
             <input type="submit"/>
         </form>
--- viewAccount.php
+++ viewAccount.php
@@ -6,7 +6,7 @@
             Full Name: <input type="text" name="name" value="<?php echo $_SESSION['name']; ?>"/><br/>
             Birthday: <input type="date" name="bday" value="<?php echo $_SESSION['bday']; ?>"/><br/>
             Sex: <input type="text" name="sex" value="<?php echo $_SESSION['sex']; ?>"/><br/>
-            Height: <input type="number" name="height" value="<?php echo $_SESSION['height']; ?>"/><br/>
+            Height: <input name="height" value="<?php echo $_SESSION['height']; ?>"/><br/>
             Weight: <input type="number" name="weight" value="<?php echo $_SESSION['weight']; ?>"/><br/>
             <input type="submit" value="Edit Info"/>
         </form>
