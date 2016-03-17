ca7818ba03533266cb36f258cd0f33c0235a2da8
Daniel Pugliese <dpugliese@wirelessprvnat-172-16-161-236.near.illinois.edu>
2016-03-17 12:42:40 -0500

    view account stuff

--- logout.php
+++ logout.php
@@ -1,5 +1,12 @@
 <?php
     session_start();
+    $_SESSION['netId'] = '';
+    $_SESSION['bday'] = '';
+    $_SESSION['first_name'] = $first_name;
+    $_SESSION['last_name'] = $last_name;
+    $_SESSION['height'] = '';
+    $_SESSION['weight'] = '';
+    $_SESSION['sex'] = '';
     session_destroy();
 
     echo "<script type='text/javascript'>window.location = '/';</script>";
