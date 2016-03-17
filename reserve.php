f168c22659b2e7fa292d910a42fd233704d3808b
dougzhu1 <dougzhu1@gmail.com>
2016-03-17 14:19:39 -0500

    debug

--- reserve.php
+++ reserve.php
@@ -29,7 +29,7 @@
 			<div id="body">
 			<div id="pagetitle">Make Your Reservation!</div>
 				<div id="content">
-					<form action="reservation.php" method="post">
+					<form action="reservations.php" method="post">
 						<input class="bigText" type="date" name="start" placeholder="Start Time (YYYY-MM-DD)"><br/>
 						<input class="bigText" type="date" name="end" placeholder="End Time (YYYY-MM-DD)"><br/>
 						<select class="bigText" name="roomID">
