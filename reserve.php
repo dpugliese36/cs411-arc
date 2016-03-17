74cf6e8cc2400a371ac54b4f50f46951d6f6f042
Daniel Pugliese <dpugliese@wirelessprvnat-172-16-161-236.near.illinois.edu>
2016-03-17 15:35:14 -0500

    trying string

--- reserve.php
+++ reserve.php
@@ -31,8 +31,8 @@
 			<div id="pagetitle">Make Your Reservation!</div>
 				<div id="content">
 					<form action="reservations.php" method="post">
-						<input class="bigText" type="datetime" name="startTime" placeholder="Start Time"><br/>
-						<input class="bigText" type="datetime" name="endTime" placeholder="End Time"><br/>
+						<input class="bigText" name="startTime" placeholder="Start Time"><br/>
+						<input class="bigText" name="endTime" placeholder="End Time"><br/>
 						<select class="bigText" name="roomID">
 							<option value="ARC205">ARC205 - All Purpose Room</option>
 							<option value="ARC214">ARC214 - All Purpose Room</option>
