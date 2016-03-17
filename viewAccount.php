856e020f9e726229a3975aa928654a268e71bba7
Daniel Pugliese <dpugliese@wirelessprvnat-172-16-161-236.near.illinois.edu>
2016-03-17 12:00:35 -0500

    adding viewAccount.php

---
+++ viewAccount.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <form>
+            Full Name: <input type="text" name="name" value="<?php echo 'test' ?>"/><br/>
+            Birthday: <input type="date" name="bday"/><br/>
+            Sex: <input type="text" name="sex"/><br/>
+            Height: <input type="number" name="height"/><br/>
+            Weight: <input type="number" name="weight"/><br/>
+            <input type="submit"/>
+        </form>
+    </body>
+</html>
