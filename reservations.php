a5155a52554723deee2690fd64eeb62eb66d88f1
dougzhu1 <dougzhu1@gmail.com>
2016-04-19 16:32:17 -0500

    debug

--- reservations.php
+++ reservations.php
@@ -16,7 +16,7 @@
     $currentDate=date_create("2016-04-21");
 
     if ($mysqli->query($sql) < 3) {
-        echo "hello world \n";
+        echo "hello worlds \n";
         $formDate = strtotime('d-m-Y',$startTime);
         $date = date('d-m-Y', $formDate);
         $diff = date_diff($date, $currentDate);
